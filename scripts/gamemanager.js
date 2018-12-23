6f37d70a3a9abb0ed2e7cb9dab9f9799ae50bd56
Sherbaz Hashmi <u6409914@anu.edu.au>
2018-12-23 16:40:40 +1100

    Functional Cleaner

--- scripts/gamemanager.js
+++ scripts/gamemanager.js
@@ -3,28 +3,22 @@ var context = canvas.getContext("2d")
 
 // Setup Images
 	var bird = this.initImage("bird.png")
-	var bg = new Image()
-	bg.src = "images/bg.png"
+	var bg = initImage("bg.png")
 	var fg = this.initImage("fg.png")
 	var pipeSky = this.initImage("pipeNorth.png")
 	var pipeGround = this.initImage("pipeSouth.png")
 
 	function initImage(fileName) {
 		var image = new Image();
-		image.src = "images" + fileName
+		image.src = "images/" + fileName
 		return image
 	};
 	
 	function draw() {
 		context.drawImage(bg,0,0);
 		
+		requestAnimationFrame(draw);
 	} 
-	
-	bg.onload = function() {
-		alert(bg.src)
-	}
-	
-	alert(bg.src)
-	
+
 	draw();
 
