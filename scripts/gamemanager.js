efe1b6dbc6827d08226ecd5c336c4dd2cd088b67
Sherbaz Hashmi <u6409914@anu.edu.au>
2018-12-23 15:33:17 +1100

    Added Resources

---
+++ scripts/gamemanager.js
@@ -0,0 +1,30 @@
+var canvas = document.getElementById("canvas")
+var context = canvas.getContext("2d")
+
+// Setup Images
+	var bird = this.initImage("bird.png")
+	var bg = new Image()
+	bg.src = "images/bg.png"
+	var fg = this.initImage("fg.png")
+	var pipeSky = this.initImage("pipeNorth.png")
+	var pipeGround = this.initImage("pipeSouth.png")
+
+	function initImage(fileName) {
+		var image = new Image();
+		image.src = "images" + fileName
+		return image
+	};
+	
+	function draw() {
+		context.drawImage(bg,0,0);
+		
+	} 
+	
+	bg.onload = function() {
+		alert(bg.src)
+	}
+	
+	alert(bg.src)
+	
+	draw();
+
